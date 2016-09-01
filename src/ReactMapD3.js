4c7f13636b18483341262d6a1f730430055ef283
Tom Fleming <tomdfleming@gmail.com>
2016-08-31 23:45:03 -0700

    added some transitions; pass react props down to d3

--- src/ReactMapD3.js
+++ src/ReactMapD3.js
@@ -5,27 +5,39 @@ let svg, html, bubble;
 export default class ReactMapD3 {
 
   constructor(el, props = {}) {
-    //
+    let defaultProps = {
+      id: 'map-d3',
+      mapfile: 'my_topo.json',
+      width: 1000,
+      height: 400
+    }
+
+    this.props = Object.assign(defaultProps, props);
+
     // reference to svg element containing circles
     this.svg = d3.select(el).append('svg')
-      .attr('class', 'map-d3')
-      .attr('width', 1000)
-      .attr('height', 400);
+      .attr('id', this.props.id)
+      .attr('width', this.props.width)
+      .attr('height', this.props.height);
 
     let svg = this.svg;
-    let width = this.svg.attr("width");
-    let height = this.svg.attr("height");
 
     let projection = d3.geoAlbersUsa()
       .scale(1280)
-      .translate([width / 2, height / 2]);
+      .translate([this.props.width / 2, this.props.height / 2]);
 
     this.path = d3.geoPath()
       .projection(projection);
     let path = this.path;
 
+    this.rateById = d3.map();
+    let that = this;
     d3.queue()
-      .defer(d3.json, "dist/us.json")
+      .defer(d3.json, this.props.mapfile)
+      .defer(
+        d3.tsv,
+        "dist/unemployment.tsv",
+        (d) => { that.rateById.set(d.id, +d.rate); })
       .await(ready);
 
     function ready(error, us) {
@@ -36,30 +48,38 @@ export default class ReactMapD3 {
         .selectAll("path")
           .data(topojson.feature(us, us.objects.counties).features)
         .enter().append("path")
-          .attr("d", path);
+          .attr("d", path)
+          .style('stroke', '#FFFFFF');
     }
   }
 
   update(el, props) {
-    var data = props;
-    if (!data) return;
 
-    this.svg.attr('width', props.width);
-    this.svg.attr('height', props.height);
+    Object.assign(this.props, props);
 
-    let width = this.svg.attr("width");
-    let height = this.svg.attr("height");
+    this.svg.
+      transition().duration(2000)
+      .attr('width', this.props.width)
+      .attr('height', this.props.height);
 
     let projection = d3.geoAlbersUsa()
       .scale(1280)
-      .translate([width / 2, height / 2]);
+      .translate([this.props.width / 2, this.props.height / 2]);
 
     this.path = d3.geoPath()
       .projection(projection);
     let path = this.path;
 
-    this.svg.selectAll('path').attr('d', path);
+    var quantize = d3.scaleQuantize()
+      .domain([0, 0.15])
+      .range(d3.range(9).map((i) => { return "q" + i + "-9"; }));
 
+    let that = this;
+    this.svg.selectAll('path')
+      .attr('d', path)
+      .attr("class", (d) => { return quantize(that.rateById.get(d.id)); })
+      .transition().duration(3000)
+      .style('stroke', '#81E797')
   }
 
   destroy(el) {}
--- src/main.js
+++ src/main.js
@@ -1,41 +1,6 @@
-import ReactMapD3 from './ReactMapD3';
+import ReactMap from './ReactMap';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let empty = () => {}
-
-class ReactMap extends React.Component {
-  render () {
-    let boundClick = this.handleClick.bind(this);
-    return <div className="map-container" onClick={boundClick}></div>;
-  }
-
-  componentDidMount () {
-    this.map = new ReactMapD3(this.getDOMNode(), this.getChartState());
-  }
-
-  componentDidUpdate () {
-    this.map.update(this.getDOMNode(), this.getChartState());
-  }
-
-  getChartState () {
-    return {
-      data: this.props.data,
-      onClick: this.props.onClick || empty
-    }
-  }
-
-  componentWillUnmount () {
-    this.map.destroy(this.getDOMNode());
-  }
-
-  getDOMNode () {
-    return ReactDOM.findDOMNode(this);
-  }
-
-  handleClick() {
-    this.map.update(this.getDOMNode(), {width: 2000, height: 1000});
-  }
-}
 var rootElement = document.getElementById('mymap');
-ReactDOM.render(<ReactMap />, rootElement);
+ReactDOM.render(<ReactMap mapfile="dist/us.json" width={500} />, rootElement);
