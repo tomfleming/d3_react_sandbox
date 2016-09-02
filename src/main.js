66318e50af9f92d04003e3b4380ed38da763b9fc
Tom Fleming <tomdfleming@gmail.com>
2016-09-01 22:41:11 -0700

    include react select box example

--- src/main.js
+++ src/main.js
@@ -1,6 +1,50 @@
 import ReactMap from './ReactMap';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactSelect from 'react-select';
+
+
+class MySelect extends ReactSelect {
+  constructor (props) {
+    super(props);
+    this.state = {value: props.value};
+  }
+
+  handleClick(val) {
+    console.log(val);
+    this.setState({value: val});
+  }
+
+  render() {
+    let boundClick = this.handleClick.bind(this);
+    return (
+      <ReactSelect
+        {...this.props}
+        value={this.state.value}
+        onChange={boundClick}>
+      </ReactSelect>
+    )
+  }
+}
+
+class App extends React.Component {
+  render () {
+    let options = [
+        { value: 'one', label: 'One' },
+        { value: 'two', label: 'Two' }
+    ];
+    return (
+      <div>
+        <ReactMap mapfile="dist/us.json" width={500} />
+        <MySelect
+          name="form-field-name"
+          value="one"
+          options={options}
+        />
+      </div>
+    )
+  }
+}
 
 var rootElement = document.getElementById('mymap');
-ReactDOM.render(<ReactMap mapfile="dist/us.json" width={500} />, rootElement);
+ReactDOM.render(<App />, rootElement);
